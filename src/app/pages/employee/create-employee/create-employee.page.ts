67476ea23b9b97a3a07aab71fe864e16c954f158
ambuj <ambuj@geoalgo.com>
2021-04-07 11:09:34 +0530

    changes create employee validation

--- src/app/pages/employee/create-employee/create-employee.page.ts
+++ src/app/pages/employee/create-employee/create-employee.page.ts
@@ -14,7 +14,7 @@ export class CreateEmployeePage implements OnInit {
 
   addEmployeeForm = this.formBuilder.group({
     id: [""],
-    name: ['',[Validators.required]],
+    name: ['',[Validators.required,Validators.minLength(2)]],
     username: [''],
     password: [''],
     confirm_password: [''],
